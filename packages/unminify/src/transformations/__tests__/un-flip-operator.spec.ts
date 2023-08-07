330461a7ea96858ed37c1ca866f16b15560e16d4
Pionxzh <spigbbbbb@gmail.com>
2023-08-08 02:45:53 +0800

    feat: improve flip operator

---
+++ packages/unminify/src/transformations/__tests__/un-flip-operator.spec.ts
@@ -0,0 +1,84 @@
+import transform from '../un-flip-operator'
+import { defineInlineTest } from './test-utils'
+
+const inlineTest = defineInlineTest(transform)
+
+inlineTest('should flip operators',
+  `
+undefined === foo;
+null !== foo;
+1 == foo;
+true != foo;
+"str" == foo;
+\`test\` == foo;
+NaN == foo;
+Infinity == foo;
+-Infinity == foo;
+"function" == typeof foo;
+
+1 < bar;
+1 > bar;
+1 <= bar;
+1 >= bar;
+`,
+  `
+foo === undefined;
+foo !== null;
+foo == 1;
+foo != true;
+foo == "str";
+foo == \`test\`;
+foo == NaN;
+foo == Infinity;
+foo == -Infinity;
+typeof foo == "function";
+
+bar > 1;
+bar < 1;
+bar >= 1;
+bar <= 1;
+`,
+)
+
+inlineTest('should not flip operators',
+  `
+foo === undefined;
+foo !== null;
+foo == 1;
+foo != true;
+foo == "str";
+foo == \`test\`;
+foo == \`test\${1}\`;
+foo == NaN;
+foo == Infinity;
+typeof foo == "function";
+
+({} == foo);
+\`test\${1}\` == foo;
+
+bar > 1;
+bar < 1.2;
+bar >= 1;
+bar <= 1;
+`,
+  `
+foo === undefined;
+foo !== null;
+foo == 1;
+foo != true;
+foo == "str";
+foo == \`test\`;
+foo == \`test\${1}\`;
+foo == NaN;
+foo == Infinity;
+typeof foo == "function";
+
+({} == foo);
+\`test\${1}\` == foo;
+
+bar > 1;
+bar < 1.2;
+bar >= 1;
+bar <= 1;
+`,
+)
---
+++ packages/unminify/src/transformations/un-flip-operator.ts
@@ -0,0 +1,96 @@
+import wrap from '../wrapAstTransformation'
+import type { ASTTransformation } from '../wrapAstTransformation'
+import type { ExpressionKind } from 'ast-types/gen/kinds'
+import type { BinaryExpression, JSCodeshift } from 'jscodeshift'
+
+const equalityOperators = [
+    '==',
+    '===',
+    '!=',
+    '!==',
+]
+const relationalOperators = [
+    '<',
+    '>',
+    '<=',
+    '>=',
+]
+
+const operatorFlipMap = new Map<BinaryExpression['operator'], BinaryExpression['operator']>([
+    ['==', '!='],
+    ['===', '!=='],
+    ['!=', '=='],
+    ['!==', '==='],
+    ['<', '>'],
+    ['>', '<'],
+    ['<=', '>='],
+    ['>=', '<='],
+])
+
+const commonValueIdentifiers = [
+    'undefined',
+    'NaN',
+    'Infinity',
+]
+
+const validLiteralTypes = ['number', 'string', 'boolean']
+const isLeftValid = (j: JSCodeshift, node: ExpressionKind) => {
+    if (j.Literal.check(node)) {
+        if (node.value === null) return true
+        return validLiteralTypes.includes(typeof node.value)
+    }
+
+    if (j.Identifier.check(node)) {
+        return commonValueIdentifiers.includes(node.name)
+    }
+
+    if (j.UnaryExpression.check(node)) {
+        return j.Identifier.check(node.argument) && commonValueIdentifiers.includes(node.argument.name)
+    }
+
+    if (j.TemplateLiteral.check(node)) {
+        return node.expressions.length === 0
+    }
+
+    return false
+}
+
+const isRightValid = (j: JSCodeshift, node: ExpressionKind) => {
+    return j.Identifier.check(node)
+     || (j.UnaryExpression.check(node) && j.Identifier.check(node.argument))
+}
+/**
+ * Flips comparisons that are in the form of "literal comes first"
+ * to "literal comes second".
+ *
+ * @example
+ * `1 < a` -> `a > 1`
+ * `undefined === foo` -> `foo === undefined`
+ * `null !== bar` -> `bar !== null`
+ *
+ * @see https://babeljs.io/docs/en/babel-plugin-minify-flip-comparisons (reversed)
+ * @see https://eslint.org/docs/latest/rules/yoda
+ * @see https://github.com/eslint/eslint/blob/main/lib/rules/yoda.js
+ */
+export const transformAST: ASTTransformation = (context) => {
+    const { root, j } = context
+
+    root
+        .find(j.BinaryExpression, {
+            operator: op => equalityOperators.includes(op) || relationalOperators.includes(op),
+        })
+        .forEach((p) => {
+            const { node } = p
+            const { operator, left, right } = node
+
+            if (isRightValid(j, right) && isLeftValid(j, left)) {
+                node.left = right
+                node.right = left
+                if (relationalOperators.includes(operator)) {
+                    node.operator = operatorFlipMap.get(operator) || operator
+                }
+            }
+        })
+}
+
+export default wrap(transformAST)
