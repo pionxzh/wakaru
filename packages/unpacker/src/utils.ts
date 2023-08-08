131e0f78603920a51c907d29a8729c0254380671
Pionxzh <pionxzh@csie.io>
2023-08-08 22:48:05 +0800

    chore: bump deps

--- packages/unpacker/src/utils.ts
+++
@@ -1,9 +0,0 @@
-import babelParser from 'prettier/parser-babel'
-import prettier from 'prettier/standalone'
-
-export function prettierFormat(code: string) {
-    return prettier.format(code, {
-        parser: 'babel',
-        plugins: [babelParser],
-    })
-}
