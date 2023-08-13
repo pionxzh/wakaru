bd6d85bef20d10dc8e7523315dbd2e65638d7c32
Pionxzh <pionxzh@csie.io>
2023-08-13 13:18:29 +0800

    chore: update rules export

--- packages/unminify/src/transformations/index.ts
+++ packages/unminify/src/transformations/index.ts
@@ -7,15 +7,19 @@ import exports from '5to6-codemod/transforms/exports.js'
 import lebab from './lebab'
 import moduleMapping from './module-mapping'
 import prettier from './prettier'
+import unAsyncAwait from './un-async-await'
 import unBoolean from './un-boolean'
+import unBracketNotation from './un-bracket-notation'
 import unEsHelper from './un-es-helper'
 import unES6Class from './un-es6-class'
 import unExportRename from './un-export-rename'
 import unFlipComparisons from './un-flip-operator'
 import unIfStatement from './un-if-statement'
+import unInfinity from './un-infinity'
 import unNumberLiteral from './un-number-literal'
 import unSequenceExpression from './un-sequence-expression'
 import unSwitchStatement from './un-switch-statement'
+import unTemplateLiteral from './un-template-literal'
 import unUseStrict from './un-use-strict'
 import unVariableMerging from './un-variable-merging'
 import unVoid0 from './un-void-0'
@@ -34,7 +38,10 @@ export const transformationMap: {
     'un-es-helper': unEsHelper,
     'un-boolean': unBoolean,
     'un-void-0': unVoid0,
+    'un-infinity': unInfinity,
     'un-number-literal': unNumberLiteral,
+    'un-template-literal': unTemplateLiteral,
+    'un-bracket-notation': unBracketNotation,
     'un-flip-comparisons': unFlipComparisons,
     'un-variable-merging': unVariableMerging,
     'un-sequence-expression2': unSequenceExpression,
@@ -42,5 +49,6 @@ export const transformationMap: {
     'un-if-statement': unIfStatement,
     'un-sequence-expression3': unSequenceExpression,
     'un-es6-class': unES6Class,
+    'un-async-await': unAsyncAwait,
     prettier,
 }
