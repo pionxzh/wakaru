25a7d4c52911541d693f094a83184cfd7fb565e5
Pionxzh <pionxzh@csie.io>
2023-10-04 01:26:30 +0800

    fix: add createForOfIteratorHelper to the list

--- packages/unminify/src/transformations/runtime-helpers/index.ts
+++ packages/unminify/src/transformations/runtime-helpers/index.ts
@@ -1,6 +1,7 @@
 import wrap from '../../wrapAstTransformation'
 import { transformAST as arrayLikeToArray } from './babel/arrayLikeToArray'
 import { transformAST as arrayWithoutHoles } from './babel/arrayWithoutHoles'
+import { transformAST as createForOfIteratorHelper } from './babel/createForOfIteratorHelper'
 import { transformAST as _extends } from './babel/extends'
 import { transformAST as objectSpread } from './babel/objectSpread'
 import { transformAST as slicedToArray } from './babel/slicedToArray'
@@ -11,10 +12,11 @@ export const transformAST: ASTTransformation = (context, params) => {
     // babel helpers
     arrayLikeToArray(context, params)
     arrayWithoutHoles(context, params)
-    objectSpread(context, params)
     toConsumableArray(context, params)
     slicedToArray(context, params)
     _extends(context, params)
+    objectSpread(context, params)
+    createForOfIteratorHelper(context, params)
 }
 
 export default wrap(transformAST)
