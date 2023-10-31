57f7fbe47728ab57aa956b901387265cb5d0d91d
Pionxzh <pionxzh@csie.io>
2023-11-01 03:24:47 +0800

    feat: add logo to header

--- packages/ide/src/components/Header.tsx
+++ packages/ide/src/components/Header.tsx
@@ -1,10 +1,17 @@
+import logo from '/icon.png?url'
+
 export function Header() {
     return (
         <div
             className="h-full"
             style={{ backgroundColor: 'rgb(40, 44, 52)' }}
         >
-            Header
+            <div
+                className="h-full w-8 bg-no-repeat bg-center bg-[length:16px]"
+                style={{
+                    backgroundImage: `url(${logo})`,
+                }}
+            />
         </div>
     )
 }
