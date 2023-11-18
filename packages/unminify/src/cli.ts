16935d5989d83ee17cce34db6d856b2717f2cad5
Pionxzh <pionxzh@csie.io>
2023-11-18 17:23:53 +0800

    feat(unminify): implement cli

--- packages/unminify/src/cli.ts
+++ packages/unminify/src/cli.ts
@@ -1,6 +1,8 @@
+#!/usr/bin/env node
+
 /* eslint-disable no-console */
 import * as path from 'node:path'
-import process from 'node:process'
+import process, { hrtime } from 'node:process'
 import fsa from 'fs-extra'
 import * as globby from 'globby'
 import c from 'picocolors'
@@ -11,59 +13,92 @@ import { runDefaultTransformation } from '.'
 import type { FileInfo } from 'jscodeshift'
 import type { Argv } from 'yargs'
 
-interface UnminifyOptions {
-    input: string
-    output?: string
-}
+type LogLevel = 'error' | 'warn' | 'info' | 'debug' | 'silent'
 
-function commonOptions(args: Argv<unknown>): Argv<UnminifyOptions> {
+function commonOptions(args: Argv<unknown>) {
     return args
-        .option('input', {
-            alias: 'i',
-            demandOption: true,
+        .option('log-level', {
             type: 'string',
-            describe: 'specify the input file or directory (glob pattern is supported)',
-        })
-        .demandOption('input', c.yellow('Please add -i to specify the input file or directory'))
-        .option('output', {
-            alias: 'o',
-            default: 'dist/',
-            type: 'string',
-            describe: 'specify the output directory (default: dist/)',
+            default: 'info',
+            choices: ['error', 'warn', 'info', 'debug', 'silent'],
+            describe: 'change the level of logging for the CLI.',
         })
 }
 
 // eslint-disable-next-line no-unused-expressions
 yargs(hideBin(process.argv))
-    .scriptName('unminify')
-    .usage('Usage: $0 -i <bundle_file> -o <out_dir>`')
-    .command(
-        '*',
-        'Unminify your bundled code',
-        args => commonOptions(args).help(),
-        async (_args) => {
-            // const exitCode = await check(await resolveConfig(args))
-            // process.exit(exitCode)
-        },
-    )
+    .scriptName('@wakaru/unminify')
+
+    .help()
     .showHelpOnFail(true)
     .alias('h', 'help')
+
     .version('version', version)
     .alias('v', 'version')
-    .help()
+
+    .usage('Usage: $0 <files...> [options]')
+    .command(
+        '* <files...>',
+        'Unminify your bundled code',
+        args => commonOptions(args)
+            .option('output', {
+                alias: 'o',
+                describe: 'specify the output directory (default: dist/)',
+                type: 'string',
+                default: 'dist/',
+            })
+            .option('force', {
+                alias: 'f',
+                describe: 'force overwrite output directory',
+                type: 'boolean',
+                default: false,
+            })
+            .positional('files', {
+                describe: 'File paths to process (supports glob patterns)',
+                type: 'string',
+                array: true,
+            })
+            .help(),
+        async (args) => {
+            await codemod(
+                args.files ?? [],
+                args.output,
+                args.force,
+                args.logLevel as LogLevel,
+            )
+        },
+    )
     .argv
 
-export async function codemod(
+async function codemod(
     paths: string[],
     output: string,
+    force: boolean,
+    logLevel: LogLevel,
 ) {
     const cwd = process.cwd()
-    const resolvedPaths = globby.sync(paths.concat('!node_modules'))
-    const outputPaths: string[] = []
+    const globbyPaths = paths
+        .map(p => path.normalize(p))
+        .map(p => p.replace(/\\/g, '/'))
+    const resolvedPaths = await globby.default(globbyPaths, {
+        cwd,
+        absolute: true,
+        ignore: [path.join(cwd, '**/node_modules/**')],
+    })
     const outputDir = path.resolve(cwd, output)
-    fsa.ensureDirSync(outputDir)
 
-    resolvedPaths.forEach(async (p) => {
+    if (fsa.existsSync(outputDir)) {
+        if (!force) {
+            throw new Error(`Output directory already exists at ${c.green(path.relative(cwd, outputDir))}. Pass ${c.yellow('--force')} to overwrite.`)
+        }
+    }
+    else {
+        fsa.ensureDirSync(outputDir)
+    }
+
+    for (const p of resolvedPaths) {
+        const start = hrtime()
+
         const source = fsa
             .readFileSync(p)
             .toString()
@@ -75,13 +110,17 @@ export async function codemod(
             source,
         }
         const result = runDefaultTransformation(fileInfo)
+        // remove leading ../
+        const safePath = path.relative(cwd, p).replace(/\.\.\\/g, '')
+        const outputPath = path.join(outputDir, safePath)
+        fsa.ensureDirSync(path.dirname(outputPath))
+        fsa.writeFileSync(outputPath, result.code)
 
-        if (source !== result.code) {
-            console.log(`Writing file: ${p}`)
-            const outputPath = path.join(outputDir, path.relative(cwd, p))
-            outputPaths.push(outputPath)
-            fsa.ensureDirSync(path.dirname(outputPath))
-            fsa.writeFileSync(outputPath, result.code)
+        if (logLevel !== 'silent') {
+            const end = hrtime(start)
+            const elapsed = end[0] * 1e9 + end[1]
+            const formattedElapsed = (elapsed / 1e6).toLocaleString('en-US', { maximumFractionDigits: 1 })
+            console.log(`${c.dim('•')} Transforming ${c.green(path.relative(cwd, p))} ${c.dim(`(${formattedElapsed}ms)`)}`)
         }
-    })
+    }
 }
--- packages/unminify/tsup.config.ts
+++ packages/unminify/tsup.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-    entry: ['src/index.ts'],
+    entry: ['src/index.ts', 'src/cli.ts'],
     format: ['cjs', 'esm'],
     dts: true,
     splitting: true,
     sourcemap: true,
     clean: true,
+    define: {
+        'process.env.NODE_DEBUG': 'undefined',
+    },
     // minify: true,
     noExternal: [
         // 'jscodeshift',
