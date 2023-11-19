e3d277770d20c11d5af09073d202fd6f4416f4c9
Pionxzh <pionxzh@csie.io>
2023-11-19 14:21:19 +0800

    feat(unpacker): implement cli

--- packages/unpacker/tsup.config.ts
+++ packages/unpacker/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-    entry: ['src/index.ts'],
+    entry: ['src/index.ts', 'src/cli.ts'],
     format: ['cjs', 'esm'],
     dts: true,
     splitting: true,
