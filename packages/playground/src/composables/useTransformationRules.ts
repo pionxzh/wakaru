26ab4093f1cc2d58ad271069c2e03d6b8b5d1353
Pig Fang <g-plane@hotmail.com>
2023-11-24 11:18:06 +0800

    feat(playground): support dnd to re-order rules

--- packages/playground/src/composables/useTransformationRules.ts
+++ packages/playground/src/composables/useTransformationRules.ts
@@ -1,9 +1,10 @@
 import { transformationMap } from '@wakaru/unminify'
+import { ref } from 'vue'
 import { KEY_DISABLED_RULES } from '../const'
 import { useLocalStorage } from './shared/useLocalStorage'
 
 export const useTransformationRules = () => {
-    const allRules = Object.keys(transformationMap)
+    const allRules = ref(Object.keys(transformationMap))
     const [disabledRules, setDisabledRules] = useLocalStorage<string[]>(KEY_DISABLED_RULES, [])
 
     return {
