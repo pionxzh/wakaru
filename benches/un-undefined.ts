5ce9c8540383a3e79bb039d2361581467784e7d6
Pionxzh <pionxzh@csie.io>
2023-12-02 13:13:44 +0800

    feat: implement `un-undefined` with `tree-sitter`

--- benches/un-undefined.ts
+++ benches/un-undefined.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env npx tsx
 
-import { dirname } from 'node:path'
+import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { add, complete, cycle, save, suite } from 'benny'
+import MagicString from 'magic-string'
+import TreeSitterParser from 'web-tree-sitter'
 import { runTransformations, transformationMap } from '../packages/unminify/src/index'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
@@ -15,12 +17,49 @@ void 99;
 `
 
 const main = async () => {
+    await TreeSitterParser.init()
+    const parser = new TreeSitterParser()
+    const lang = await TreeSitterParser.Language.load(join(__dirname, './tree-sitter-javascript.wasm'))
+    parser.setLanguage(lang)
+
     await suite(
         title,
-        ...([10, 100, 1000, 5000].map((count) => {
+        ...([10, 100, 1000].map((count) => {
             const source = snippet.repeat(count)
             return add(`items=${count}`, () => {
                 runTransformations({ path: '', source }, [rule])
+            })
+        })),
+        ...([10, 100, 1000, 5000].map((count) => {
+            const source = snippet.repeat(count)
+            return add(`tree=${count}`, async () => {
+                const und = 'undefined'
+                const s = new MagicString(source)
+                const tree = parser.parse(source)
+
+                const query = lang.query(`
+                (unary_expression
+                    operator: "void"
+                    argument: [
+                        (number)
+                        (parenthesized_expression
+                            (number)
+                        )
+                    ]
+                ) @target
+                `)
+
+                query.matches(tree.rootNode).forEach((match) => {
+                    match.captures.forEach((capture) => {
+                        const node = capture.node
+                        s.update(node.startIndex, node.endIndex, und)
+                    })
+                })
+
+                // query.delete()
+                // tree.delete()
+
+                return s.toString()
             })
         })),
         cycle(),
