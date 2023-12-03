770edecdf5885c7ca287616c59ae0c72eaab60a9
Pionxzh <pionxzh@csie.io>
2023-12-03 17:20:45 +0800

    fix(playground): adjust style

--- packages/playground/src/components/Header.vue
+++ packages/playground/src/components/Header.vue
@@ -7,7 +7,7 @@ import DarkModeBtn from './DarkModeBtn.vue'
     <header
         class="flex h-16 items-center py-4 pl-6 pr-8
         text-base text-semibold
-        bg-[#f2f2f2] dark:bg-gray-800
+        bg-[#e5e5e5] dark:bg-gray-800
         border-b border-gray-300 dark:border-gray-700"
     >
         <img :src="logo" alt="logo" class="w-6 h-6 mr-2">
