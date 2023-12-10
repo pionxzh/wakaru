deda1df1c2894c7e9b2b443c01033d366eec549c
Pionxzh <pionxzh@csie.io>
2023-12-10 21:37:01 +0800

    feat(cli): implement new standalone CLI with better UX

---
+++ packages/cli/src/unminify.ts
@@ -0,0 +1,38 @@
+/* eslint-disable no-console */
+import * as path from 'node:path'
+import process from 'node:process'
+import { runTransformations, transformationRules } from '@wakaru/unminify'
+import fsa from 'fs-extra'
+import { Timing } from './perf'
+import type { Transform } from 'jscodeshift'
+
+export async function unminify(
+    paths: string[],
+    baseDir: string,
+    outputDir: string,
+    perf: boolean,
+) {
+    await fsa.ensureDir(outputDir)
+
+    const cwd = process.cwd()
+    const timing = new Timing(perf)
+
+    for (const p of paths) {
+        const outputPath = path.join(outputDir, path.relative(baseDir, p))
+        const filename = path.relative(cwd, outputPath)
+        const measure = <T>(key: string, fn: () => T) => timing.collect(filename, key, fn)
+        const measureAsync = <T>(key: string, fn: () => Promise<T>) => timing.collectAsync(filename, key, fn)
+
+        await timing.measureTimeAsync(async () => {
+            const source = await measureAsync('read file', () => fsa.readFile(p, 'utf-8'))
+
+            const transformations = transformationRules.map<Transform>((rule) => {
+                const { id, transform } = rule
+                return (...args: Parameters<Transform>) => measure(id, () => transform(...args))
+            })
+            const result = measure('runDefaultTransformation', () => runTransformations({ path: p, source }, transformations))
+
+            await measureAsync('write file', () => fsa.writeFile(outputPath, result.code, 'utf-8'))
+        })
+    }
+}
