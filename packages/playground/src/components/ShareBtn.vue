3c026fb6ddeb1be88efd28df55c37ad818a72eee
Pionxzh <pionxzh@csie.io>
2024-01-21 10:37:18 +0800

    fix(playground): share button should align right edge

--- packages/playground/src/components/ShareBtn.vue
+++ packages/playground/src/components/ShareBtn.vue
@@ -3,7 +3,7 @@
 
 <template>
     <button
-        class="absolute top-0 right-6 flex items-center p-2 text-xs font-medium rounded-lg outline-none
+        class="absolute top-0 right-0 flex items-center p-2 text-xs font-medium rounded-lg outline-none
         text-gray-700 dark:text-gray-200
         bg-gray-200 dark:bg-gray-700
         hover:bg-gray-300 dark:hover:bg-gray-600"
