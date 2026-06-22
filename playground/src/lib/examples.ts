92109d7968c686e77b8d160e71ca8bfe5c022a09
pionxzh <pionxzh@csie.io>
2026-06-22 20:27:49 +0800

    chore(playground): update playground example

--- playground/src/lib/examples.ts
+++ playground/src/lib/examples.ts
@@ -1,63 +1,101 @@
 export const DEFAULT_EXAMPLE = `\
-function _classCallCheck(a,b){if(!(a instanceof b))throw new TypeError("Cannot call a class as a function")}function _defineProperties(a,b){for(var c=0;c<b.length;c++){var d=b[c];d.enumerable=d.enumerable||!1,d.configurable=!0,"value"in d&&(d.writable=!0),Object.defineProperty(a,d.key,d)}}function _createClass(a,b,c){return b&&_defineProperties(a.prototype,b),c&&_defineProperties(a,c),a}
+"use strict";
+var _classCallCheck = require("@babel/runtime/helpers/classCallCheck");
+var _createClass = require("@babel/runtime/helpers/createClass");
+var _asyncToGenerator = require("@babel/runtime/helpers/asyncToGenerator");
+var _slicedToArray = require("@babel/runtime/helpers/slicedToArray");
+var React = require("react");
 
-var Store = function() {
-    function Store(a) {
-        _classCallCheck(this, Store);
-        this.name = a !== null && a !== void 0 ? a : "default";
-        this.items = [];
+function formatResult(a) {
+    var b = a.label;
+    var c = a.score;
+    var d = a.meta;
+    var _d;
+    var e = (_d = d === null || d === void 0 ? void 0 : d.tag) !== null && _d !== void 0 ? _d : "none";
+    return "[".concat(e, "] ").concat(b, ": ").concat(Math.pow(c, 2));
+}
+
+var d = function(e) {
+    var t = e.children, n = e.className;
+    var h = _slicedToArray((0, React.useState)(""), 2);
+    var g = h[0], y = h[1];
+    var z = (0, React.useRef)(null);
+    return ((0, React.useEffect)(function() {
+        y(n);
+    }, [n]),
+    React.createElement("div", { className: "".concat(n, " ").concat(g), ref: z }, t));
+};
+d.displayName = "StatusPanel";
+
+function processAll(a) {
+    var b = [];
+    for (var i = 0, c = a; i < c.length; i++) {
+        var d = c[i];
+        var _d;
+        var e = (_d = d === null || d === void 0 ? void 0 : d.score) !== null && _d !== void 0 ? _d : 0;
+        e > 0 && b.push(formatResult(d));
     }
-    return _createClass(Store, [{
+    return b;
+}
+
+var summarize = function(a) {
+    var b = a.filter(function(c) { return c.ok === !0; });
+    var c = b.map(function(d) {
+        var e = d.data, f = d.ok;
+        return { data: e, status: f ? "pass" : "fail" };
+    });
+    console.log("Done: ".concat(c.length, "/").concat(a.length, " passed"));
+    return c;
+};
+
+var TaskRunner = function() {
+    function TaskRunner(a) {
+        _classCallCheck(this, TaskRunner);
+        var _a;
+        this.name = (_a = a) !== null && _a !== void 0 ? _a : "default";
+        this.tasks = [];
+    }
+    return _createClass(TaskRunner, [{
         key: "add",
         value: function(a) {
             var b = arguments.length > 1 && arguments[1] !== void 0 ? arguments[1] : 1;
-            this.items.push({ name: a, qty: b, added: Date.now() });
-            console.log("[".concat(this.name, "] Added ").concat(b, "x ").concat(a));
+            this.tasks.push({ name: a, priority: b, ts: Date.now() });
+            console.log("[".concat(this.name, "] Added: ").concat(a, " (p=").concat(b, ")"));
         }
     }, {
-        key: "find",
+        key: "run",
         value: function(a) {
-            return this.items.filter(function(b) { return b.name === a; });
-        }
-    }, {
-        key: "summary",
-        value: function() {
-            var a = this.items;
-            var b = a.reduce(function(c, d) { return c + d.qty; }, 0);
-            return { store: this.name, count: a.length, total: b };
+            return _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+                var response, data;
+                return regeneratorRuntime.wrap(function _callee$(_context) {
+                    while (1) switch (_context.prev = _context.next) {
+                        case 0:
+                            _context.prev = 0;
+                            _context.next = 3;
+                            return fetch("/api/tasks/".concat(a.name));
+                        case 3:
+                            response = _context.sent;
+                            _context.next = 6;
+                            return response.json();
+                        case 6:
+                            data = _context.sent;
+                            return _context.abrupt("return", { ok: !0, data: data });
+                        case 8:
+                            _context.prev = 8;
+                            _context.t0 = _context["catch"](0);
+                            return _context.abrupt("return", { ok: !1, error: "Task ".concat(a.name, " failed") });
+                        case 11:
+                        case "end":
+                            return _context.stop();
+                    }
+                }, _callee, null, [[0, 8]]);
+            }))();
         }
     }]);
 }();
 
-function getLabel(a) {
-    var _a$meta;
-    var b = (_a$meta = a === null || a === void 0 ? void 0 : a.meta) !== null && _a$meta !== void 0 ? _a$meta : {};
-    var c = b.label;
-    var d = b.priority;
-    var e = c !== null && c !== void 0 ? c : "Untitled";
-    var f = d !== null && d !== void 0 ? d : 0;
-    return "".concat(e, " (priority: ").concat(f, ")");
-}
-
-function validate(a) {
-    var _a;
-    if ((_a = a) !== null && _a !== void 0 && (_a = _a.config) !== null && _a !== void 0 && _a.strict) {
-        var rules = a.rules;
-        for (var i = 0; i < rules.length; i++) {
-            var rule = rules[i];
-            if (rule.enabled === !1) console.warn("Rule " + rule.name + " is disabled");
-        }
-    }
-}
-
-var processAll = function(a) {
-    var b = a.filter(function(c) { return c.qty > 0; });
-    var c = b.map(function(d) {
-        var e = d.name, f = d.qty;
-        return { name: e, total: Math.pow(f, 2), label: getLabel(d) };
-    });
-    var d = c.reduce(function(e, f) { return e + f.total; }, 0);
-    console.log("Processed " + c.length + " items, sum: " + d);
-    return { items: c, sum: d };
-};
+var runner = new TaskRunner("demo");
+runner.add("deploy", 2);
+console.log(processAll(runner.tasks));
+console.log(summarize([{ ok: !0, data: { label: "test", score: 5 } }]));
 `;
